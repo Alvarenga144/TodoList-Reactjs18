0c2cd97cdce3abbaf8a770bd86de1ae03d817c78
Esteban Alvarenga <estebanalvarenga2002@gmail.com>
2023-06-20 23:08:28 -0600

    Footer Made with love by me :)

---
+++ src/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Footer.css';
+
+function Footer() {
+    return (
+        <p className="futercito">
+            Made with ♥️ by 
+            <a href="#">Esteban Alvarenga</a>    
+        </p>
+    );
+}
+
+export { Footer };
